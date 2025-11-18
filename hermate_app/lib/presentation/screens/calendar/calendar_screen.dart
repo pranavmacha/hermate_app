c66d9f25014fbbbc3ab6ef34db5895043294decc
pranavmacha <pranavmacha090@gmail.com>
2025-11-19 00:42:20 +0530

    Add symptom logging flow

--- hermate_app/lib/presentation/screens/calendar/calendar_screen.dart
+++ hermate_app/lib/presentation/screens/calendar/calendar_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:hive/hive.dart';
+
+import 'package:hermate_app/features/calendar/screens/symptom_log_screen.dart';
+
 import '../../../core/constants/colors.dart';
 import 'log_history_screen.dart';
 
@@ -507,6 +510,21 @@ class _CalendarScreenState extends State<CalendarScreen> {
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          final day = selectedDay ?? DateTime.now();
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => SymptomLogScreen(
+                selectedDate: day,
+                box: box,
+              ),
+            ),
+          );
+        },
+        label: const Text("Log Symptoms"),
+        icon: const Icon(Icons.favorite),
       ),
     );
   }
